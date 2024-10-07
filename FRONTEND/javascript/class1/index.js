d7b5b6b0d91b234d08fae302f21cac9f9c1a895f
satyaranjan das <satyaranjandas256@gmail.com>
2024-10-07 11:18:59 +0530

    basic operation

--- FRONTEND/javascript/class1/index.js
+++ FRONTEND/javascript/class1/index.js
@@ -1,1 +1,57 @@
-console.log("soumya");
+
+
+
+console.log(address);
+
+var address = "Bhubaneswar";
+
+console.log(address);
+//NULL
+//let user;
+
+let user =null;
+//var user=null;
+//const user=null;
+console.log(typeof null); //object
+let age = 23;
+console.log("age");
+// undefined
+let data;
+console.log(typeof data); //undefined
+console.log(data);
+//Number
+let num1 =50;
+let decimal_no=1.0001;
+let pi=3.14;
+let ge = 25;
+let roll_no = 21;
+
+//console.log(typeof null); //number
+
+
+//addition
+let num2=100;
+let num3=125;
+let addition = num2 + num3;
+console.log(addition);
+
+//Substraction
+
+let num4=300;
+let num5=200;
+let substraction = num4 - num5;
+console.log(substraction);
+
+//multipilication
+
+let num6=11;
+let num7=11;
+let mulltiplication = num6 * num7;
+console.log(mulltiplication);
+
+//division
+
+let num8=400;
+let num9=20;
+let division = num8/num9;
+console.log(division);
